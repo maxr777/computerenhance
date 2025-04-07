429cd103efcdab8c88a624fb72558213a96325ab
skibidi toilet rizz ohio <maxr.0307@gmail.com>
2025-04-07 16:23:12 +0200

    Change the way the binary is read

--- 8068.cpp
+++ 8068.cpp
@@ -3,29 +3,30 @@
 #include <fstream>
 #include <iostream>
 
-uint16_t little_endian(uint16_t a) {
-  return ((a >> 8) | (a << 8));
-}
-
 int main(int argc, char *argv[]) {
-  std::uint16_t instruction{};
+  std::uint8_t instruction{};
   std::ifstream binary(argv[1], std::ios::binary);
-  std::ofstream output("output.txt");
   std::string asm_instruction{};
+  
+  while (binary.read(reinterpret_cast<char *>(&instruction), 1)) {
+    // std::bitset<8> instruction_binary{instruction};
+    // std::cout << instruction_binary << '\t';
 
-  while (binary.read(reinterpret_cast<char *>(&instruction), 2)) {
-    // std::bitset<16> instruction_binary{instruction};
-    // std::cout << instruction_binary << '\t' << static_cast<std::bitset<16>>(little_endian(instruction)) << '\n';
-    instruction = little_endian(instruction);
-    if (((instruction & 0xFC00) >> 10) == 0x22) {
+    if (((instruction & 0xFC) >> 2) == 0x22) {
+      bool d{static_cast<bool>(instruction & 0x02)}; // 0 = source in REG, 1 = destination in REG
+      bool w{static_cast<bool>(instruction & 0x01)}; // 0 = byte operands, 1 = word
+
+      binary.read(reinterpret_cast<char *>(&instruction), 1);
+
+      // instruction_binary = instruction;
+      // std::cout << instruction_binary << '\n';
+
       asm_instruction += "mov ";
-      bool d{static_cast<bool>(instruction & 0x0200)}; // 0 = source in REG, 1 = destination in REG
-      bool w{static_cast<bool>(instruction & 0x0100)}; // 0 = byte operands, 1 = word
-      int mod{(instruction & 0x00C0) >> 6};            // mode field (memory mode no displacement, 8-bit displacement, 16-bit, register mode)
-      int reg{(instruction & 0x0038) >> 3};            // register encoding
-      int rm{instruction & 0x0007};                    // register or memory encoding
+      int mod{(instruction & 0xC0) >> 6};            // mode field (memory mode no displacement, 8-bit displacement, 16-bit, register mode)
+      int reg{(instruction & 0x38) >> 3};            // register encoding
+      int rm{instruction & 0x07};                    // register or memory encoding
 
-      std::cout << reg << '\t' << rm << '\n';
+      // std::cout << reg << '\t' << rm << '\n';
       std::string register_field{};
       if (w) {
         switch (reg) {
@@ -162,7 +163,7 @@ int main(int argc, char *argv[]) {
         asm_instruction += register_field;
       }
 
-      output << asm_instruction << '\n';
+      std::cout << asm_instruction << '\n';
       asm_instruction.clear();
     }
   }
